cd5a52ee8dfdadd999b4264e424d4e6c9d8d8257
Alexander Sisco <36649949+alexandersisco@users.noreply.github.com>
2025-05-30 22:23:19 -0700

    add ability to read a path from stdin

--- main.go
+++ main.go
@@ -1,10 +1,29 @@
 package main
 
 import "fmt"
+import "bufio"
+import "io"
+import "os"
 import "strings"
 
 func main() {
 	path := "~/workspace/goproj/pathy/main.go"
+
+	fi, err := os.Stdin.Stat()
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	if (fi.Mode() & os.ModeCharDevice) != 0 {
+		// Stdin is connected to a terminal
+	} else if (fi.Mode() & os.ModeNamedPipe) != 0 {
+		// Stdin is connected to a pipe
+		path, _ = ReadStdIn()
+	} else {
+		// Stdin is a redirected file
+		path, _ = ReadStdIn()
+	}
 
 	// ListPathSegments(path)
 
@@ -32,5 +51,19 @@ func ListPathSegments(path string) {
 
 	for i, v := range segments {
 		fmt.Println(i, v)
+	}
+}
+
+func ReadStdIn() (string, error) {
+	rdr := bufio.NewReader(os.Stdin)
+
+	switch line, err := rdr.ReadString('\n'); err {
+	case nil:
+		return line, nil
+	case io.EOF:
+		return "", err
+	default:
+		fmt.Fprintln(os.Stderr, "error:", err)
+		return "", err
 	}
 }
